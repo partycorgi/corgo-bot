b4f91ab4a717d455e3eceffc73397d36f9b5de51
Prince Wilson <prince.wilson@newsela.com>
2020-08-15 13:07:29 -0400

    Add rudimentary channel cohort creation

--- rust-bot/src/main.rs
+++ rust-bot/src/main.rs
@@ -1,40 +1,83 @@
-use std::env;
+use std::{dbg, env};
 
 use serenity::{
-    model::{channel::Message, gateway::Ready},
+    framework::standard::macros::{command, group, check},
+    framework::standard::{
+         Args, CheckResult, StandardFramework, CommandOptions, CommandResult},
+    model::{channel::GuildChannel, channel::Message, channel::ChannelType},
     prelude::*,
 };
 
-struct Handler;
+#[group]
+#[commands(ping)]
+struct General;
 
-impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping-corgo-rust" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong from Rust!") {
-                println!("Error sending message: {:?}", why);
+#[group]
+#[checks(Mod)]
+#[commands(create_cohort)]
+struct Mod;
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+        println!("Error sending message: {}", why);
+    }
+    return Ok(());
+}
+
+// This command provisions out a channel with permissions
+// to read/write for users with a role that matches the
+// channel.
+#[command]
+fn create_cohort(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    let cohort_name = args.single::<String>().unwrap();
+    create_role(ctx, msg, &cohort_name);
+    create_channel(ctx, msg, &cohort_name);
+    
+    Ok(())
+}
+
+fn create_role(ctx: &mut Context, msg: &Message, role_name: &str) {
+    let start_up_msg = "Spinning up Adventure Club...";
+    if let Err(error) = msg.channel_id.say(&ctx.http, start_up_msg) {
+        println!("Error sending message: {:?}", error);
+    };
+
+    if let Some(guild) = msg.guild(&ctx.cache) {
+        match guild.read().role_by_name(role_name) {
+            Some(role) => {
+                let content = format!("{} Role already exists!", role.name);
+                if let Err(error) = msg.channel_id.say(&ctx.http, content) {
+                    println!("{:?}", error);
+                };
+            },
+            None => {
+                guild.read().create_role(&ctx, |r| r.name(role_name)).unwrap();
+            }
+        };
+    };
+}
+
+fn create_channel(ctx: &mut Context, msg: &Message, cohort_name: &str) {
+    let channel_name = format!("adventure-club: {}", cohort_name);
+    if let Some(guild) = msg.guild(&ctx.cache) {
+
+        match guild.read().create_channel(&ctx, |c| c.name(channel_name)
+            .kind(ChannelType::Text)
+        ) {
+            Ok(channel) => {
+                println!("Created channel");
+            },
+            Err(err) => {
+                println!("Channel already exists");
             }
         }
     }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
 }
+
+struct Handler;
+
+impl EventHandler for Handler {}
 
 fn main() {
     // Configure the client with your Discord bot token in the environment.
@@ -45,6 +88,15 @@ fn main() {
     // by Discord for bot users.
     let mut client = Client::new(&token, Handler).expect("Err creating client");
 
+    // When using the framework, we just declare any custom configurations
+    // - Adds a prefix to all commands
+    // - Grabs all commands in groups
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP)
+        .group(&MOD_GROUP)
+    );
+
     // Finally, start a single shard, and start listening to events.
     //
     // Shards will automatically attempt to reconnect, and will perform
@@ -52,4 +104,16 @@ fn main() {
     if let Err(why) = client.start() {
         println!("Client error: {:?}", why);
     }
+}
+
+#[check]
+#[name = "Mod"]
+fn mod_check(ctx: &mut Context, msg: &Message, _: &mut Args, _: &CommandOptions) -> CheckResult {
+    if let Some(member) = msg.member(&ctx.cache) {
+        if let Ok(permissions) = member.permissions(&ctx.cache) {
+            return permissions.administrator().into();
+        }
+    }
+
+    false.into()
 }
