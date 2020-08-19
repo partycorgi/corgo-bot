1cf61c5bc507805563d0ad6cd29937909c8d24cd
Christopher Biscardi <chris@christopherbiscardi.com>
2020-08-19 15:44:56 -0700

    help

--- rust-bot/src/main.rs
+++ rust-bot/src/main.rs
@@ -1,20 +1,23 @@
 use std::env;
 
 use libhoney;
+use std::collections::HashSet;
 use tracing::{error, info, instrument};
 use tracing_honeycomb;
 use tracing_subscriber::layer::SubscriberExt;
 
 use serenity::{
     framework::standard::macros::{check, command, group},
-    framework::standard::{Args, CheckResult, CommandOptions, CommandResult, StandardFramework},
+    framework::standard::{
+        help_commands, macros::help, Args, CheckResult, CommandGroup, CommandOptions,
+        CommandResult, HelpOptions, StandardFramework,
+    },
     model::{
-        channel::{
-            ChannelType, GuildChannel, Message, PermissionOverwrite, PermissionOverwriteType,
-        },
+        channel::{ChannelType, GuildChannel, PermissionOverwrite, PermissionOverwriteType},
         guild::Role,
         id::RoleId,
         permissions::Permissions,
+        prelude::{Message, UserId},
     },
     prelude::*,
     utils::MessageBuilder,
@@ -29,6 +32,49 @@ struct General;
 #[checks(Mod)]
 #[commands(create_cohort)]
 struct Mod;
+
+// The framework provides two built-in help commands for you to use.
+// But you can also make your own customized help command that forwards
+// to the behaviour of either of them.
+#[help]
+// This replaces the information that a user can pass
+// a command-name as argument to gain specific information about it.
+#[individual_command_tip = "Hello! こんにちは！Hola! Bonjour! 您好!\n\
+If you want more information about a specific command, just pass the command as argument."]
+// Some arguments require a `{}` in order to replace it with contextual information.
+// In this case our `{}` refers to a command's name.
+#[command_not_found_text = "Could not find: `{}`."]
+// Define the maximum Levenshtein-distance between a searched command-name
+// and commands. If the distance is lower than or equal the set distance,
+// it will be displayed as a suggestion.
+// Setting the distance to 0 will disable suggestions.
+#[max_levenshtein_distance(3)]
+// When you use sub-groups, Serenity will use the `indention_prefix` to indicate
+// how deeply an item is indented.
+// The default value is "-", it will be changed to "+".
+#[indention_prefix = "+"]
+// On another note, you can set up the help-menu-filter-behaviour.
+// Here are all possible settings shown on all possible options.
+// First case is if a user lacks permissions for a command, we can hide the command.
+#[lacking_permissions = "Strike"]
+// If the user is nothing but lacking a certain role, we just display it hence our variant is `Nothing`.
+#[lacking_role = "Strike"]
+// The last `enum`-variant is `Strike`, which ~~strikes~~ a command.
+#[wrong_channel = "Strike"]
+// Serenity will automatically analyse and generate a hint/tip explaining the possible
+// cases of ~~strikethrough-commands~~, but only if
+// `strikethrough_commands_tip_{dm, guild}` aren't specified.
+// If you pass in a value, it will be displayed instead.
+fn my_help(
+    context: &mut Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    help_commands::with_embeds(context, msg, args, help_options, groups, owners).into()
+}
 
 #[instrument(skip(ctx))]
 #[command]
@@ -185,6 +231,7 @@ fn main() {
     client.with_framework(
         StandardFramework::new()
             .configure(|c| c.prefix("!"))
+            .help(&MY_HELP)
             .group(&GENERAL_GROUP)
             .group(&MOD_GROUP),
     );
